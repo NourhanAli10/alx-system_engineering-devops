9ac75d8751ae4157a777a907c9eaa87616cfc865
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2023-11-25 00:42:10 +0200

    task11

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int infinite_while(void);
+
+/**
+* main - Entry point
+* Return: int
+*/
+
+int main(void)
+{
+	pid_t zombie_pid;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		zombie_pid = fork();
+
+		if (zombie_pid > 0)
+		{
+			wait(NULL);
+		}
+		else
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+		}
+	}
+	infinite_while();
+
+	return (0);
+}
+
+/**
+* infinite_while - Function that runs an infinite loop
+* Return: 0 (never actually reached)
+*/
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}

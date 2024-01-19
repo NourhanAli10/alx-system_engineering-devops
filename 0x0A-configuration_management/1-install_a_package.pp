5788b4be1a55bc8eb5b27df03002214d38e8e150
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-01-19 23:21:14 +0200

    tasks

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,6 +5,11 @@ package { 'Flask':
   provider => 'pip3',
 }
 
+package { 'Werkzeug':
+  ensure   => '2.1.1',  # Specify a version that is compatible with Flask 2.1.0
+  provider => 'pip3',
+}
+
 exec { 'Refresh the shell':
   command     => 'exec $SHELL',
   path        => ['/bin', '/usr/bin', '/usr/local/bin'],

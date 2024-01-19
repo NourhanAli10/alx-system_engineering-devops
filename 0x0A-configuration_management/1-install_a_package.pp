3ab9ebaa6138379e593ce9cf8676800325c138b9
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-01-19 23:16:04 +0200

    rasks

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# install Flask -v 2.1.0
+#Install flask version 2.1.0
 
-exec { 'Flask':
-  command => '/usr/bin/apt-get -y install Flask -v 2.1.0',
+
+exec {'pip3 install flask':
+require => Exec['python-installed'],
+command => '/usr/bin/pip3 install flask==2.1.0'
+}
+
+exec {'python-installed':
+command => '/usr/bin/which python3'
 }

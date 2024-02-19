100521efd744f4e12849854ed242bd612a483d36
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-02-19 21:49:15 +0200

    tasks

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""this module  to export data in the CSV format"""
+
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    user_id = int(sys.argv[1])
+    url = f"{base_url}/{user_id}"
+    user_data = requests.get(url).json()
+    user_name = user_data["name"]
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    file_name = "{}.csv".format(user_id)
+    data = []
+    for task in todos:
+        if task.get("userId") == int(user_id):
+            user_info = [user_id, user_name,
+                         task.get("completed"), task.get("title")]
+            data.append(user_info)
+    with open(file_name, "w", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(data)

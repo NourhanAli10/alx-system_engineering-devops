29fffd47207a24660484860fe1ab8df252f59915
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-03-05 19:46:57 +0200

    tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,20 +5,13 @@ import requests
 
 def number_of_subscribers(subreddit):
     # Reddit API endpoint for subreddit information
-    username = 'nourhan1414'
-    refresh_token = "89041561833551-aB7ymnQsR1I6Oigfk63YrTnI-XYOsA"
-    user_pass_dict = {'user': username,'api_type': 'json'}
-    headers = {'user-agent': '/u/nourhan1414 API Python',
-               'Authorization': f'Bearer {refresh_token}'}
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': ' u/IZMmFoTgvneF469yS8ZVjA'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'api'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    elif response.status_code == 400:
+    if response.status_code == 404:
         return 0
-    else:
-        return 0
+    data = response.json().get("data")
+    subscribers = data.get('subscribers')
+    return subscribers
+

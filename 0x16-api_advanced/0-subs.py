4e3f7331a7974bfd84f04adde56b9cca1a211166
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-03-05 19:07:18 +0200

    tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     # Reddit API endpoint for subreddit information
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'.format(subreddit)
     headers = {'User-Agent': 'CustomUserAgent'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)

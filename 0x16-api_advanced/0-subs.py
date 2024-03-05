9c42717598dcecf884b606a109c8e36375b57ed5
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-03-05 19:10:25 +0200

    tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,43 +1,37 @@
-# #!/usr/bin/python3
-# """sub.py"""
-# import requests
-
-
-# def number_of_subscribers(subreddit):
-#     # Reddit API endpoint for subreddit information
-#     url = f'https://www.reddit.com/r/{subreddit}/about.json'.format(subreddit)
-#     headers = {'User-Agent': 'CustomUserAgent'}
-
-#     response = requests.get(url, headers=headers, allow_redirects=False)
-#     if response.status_code == 200:
-#         data = response.json()
-#         subscribers = data['data']['subscribers']
-#         return subscribers
-#     elif response.status_code == 400:
-#         return 0
-#     else:
-#         return 0
-#!/usr/bin/python3
-"""
-Function that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit.
-If an invalid subreddit is given, the function should return 0
-"""
-
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """
-    Function that queries the Reddit API
-    - If not a valid subreddit, return 0.
-    """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-    )
+    # Reddit API endpoint for subreddit information
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    
+    # Set a custom User-Agent to avoid Too Many Requests error
+    headers = {'User-Agent': 'Custom User Agent'}
 
-    if req.status_code == 200:
-        return req.json().get("data").get("subscribers")
-    else:
+    # Make a GET request to the Reddit API without following redirects
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        
+        # Extract the number of subscribers from the response
+        subscribers = data['data']['subscribers']
+        
+        return subscribers
+    elif response.status_code == 404:
+        # Invalid subreddit returns a 404 status code
         return 0
+    else:
+        # Handle other errors
+        return 0
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        subscribers = number_of_subscribers(subreddit)
+        print(f"{subreddit} has {subscribers} subscribers.")

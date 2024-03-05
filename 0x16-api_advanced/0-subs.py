827ada42961caa847401e49b435dce97297bfe55
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-03-05 19:20:38 +0200

    tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,37 +1,19 @@
+#!/usr/bin/python3
+"""sub.py"""
 import requests
+
 
 def number_of_subscribers(subreddit):
     # Reddit API endpoint for subreddit information
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    
-    # Set a custom User-Agent to avoid Too Many Requests error
-    headers = {'User-Agent': 'Custom User Agent'}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'.format(subreddit)
+    headers = {'User-Agent': ' u/IZMmFoTgvneF469yS8ZVjA'}
 
-    # Make a GET request to the Reddit API without following redirects
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check if the request was successful (status code 200)
     if response.status_code == 200:
-        # Parse the JSON response
         data = response.json()
-        
-        # Extract the number of subscribers from the response
         subscribers = data['data']['subscribers']
-        
         return subscribers
-    elif response.status_code == 404:
-        # Invalid subreddit returns a 404 status code
+    elif response.status_code == 400:
         return 0
     else:
-        # Handle other errors
         return 0
-
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit = sys.argv[1]
-        subscribers = number_of_subscribers(subreddit)
-        print(f"{subreddit} has {subscribers} subscribers.")

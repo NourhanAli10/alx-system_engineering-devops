04bd6a71f0e806bd32892d1339afeb0a01bfe24f
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-03-06 19:34:54 +0200

    tasks

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,15 +2,7 @@
 
 # Execute command to fix the issue
 exec { 'fix-apache-500-error':
-  command     => '/bin/chmod 755 /path/to/missing/file',  # Example fix: adjusting file permissions
-  path        => ['/bin', '/usr/bin'],  # Adjust the path as necessary
+  command     => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php', 
+  path        => '/usr/local/bin/:/bin/' 
   refreshonly => true,
-  subscribe   => File['/path/to/missing/file'],  # Trigger the execution only when the file changes
-}
-
-# Ensure the Apache service is restarted after applying the fix
-service { 'apache2':
-  ensure  => 'running',
-  enable  => true,
-  require => Exec['fix-apache-500-error'],  # Ensure Apache restarts after fixing the issue
 }

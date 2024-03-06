3a5637f8163351dd202c402c4e97d0ec7712bdb9
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-03-06 19:36:45 +0200

    tasks

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-# File: 0-strace_is_your_friend.pp
-exec { 'fix-apache-500-error':
-  command     => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php', 
-  path        => '/usr/local/bin/:/bin/' 
-  refreshonly => true,
+# Fix setting in Wordpress
+exec { 'settingPress':
+  command  => 'sed -i "s/\b.phpp\b/.php/g" /var/www/html/wp-settings.php',
+  provider => shell,
 }

14ffa120a315f5a5ff98777108211b040b8c727e
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-03-07 00:27:22 +0200

    tasks

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # 0-strace_is_your_friend.pp
-exec { '500error':
+exec { '500-error':
   command  => 'sed -i "s/\b.phpp\b/.php/g" /var/www/html/wp-settings.php',
   provider => '/usr/local/bin/:/bin/',
 }
